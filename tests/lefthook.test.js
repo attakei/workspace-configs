77ec056790fc9a98e4aca973e76924f1b5af78fb
Kazuya Takei <myself@attakei.net>
2025-05-14 03:47:17 +0900

    ci: Refactor tests

--- tests/lefthook.test.js
+++ tests/lefthook.test.js
@@ -5,18 +5,20 @@ import yaml from "yaml";
 import renovateConfig from "../renovate/lefthook.json";
 
 describe("YAML monitor", () => {
-  test("JSONata check", async () => {
-    const data = yaml.parse(`
+  describe("matchString (try JSONata)", () => {
+    test("Valid", async () => {
+      const data = yaml.parse(`
       remotes:
         - git_url: 'https://github.com/attakei/workspace-configs'
           ref: 'v0.2.0'
           configs:
             - 'projects/sphinx-doc/lefthook.yaml'
     `);
-    const exp = jsonata(renovateConfig.customManagers[0].matchStrings[0]);
-    const result = await exp.evaluate(data);
-    expect(result).toHaveLength(1);
-    expect(result[0].depName).toBe("attakei/workspace-configs");
-    expect(result[0].currentValue).toBe("v0.2.0");
+      const exp = jsonata(renovateConfig.customManagers[0].matchStrings[0]);
+      const result = await exp.evaluate(data);
+      expect(result).toHaveLength(1);
+      expect(result[0].depName).toBe("attakei/workspace-configs");
+      expect(result[0].currentValue).toBe("v0.2.0");
+    });
   });
 });
