4c43fa46b7ddb33a30bccec732eff6ac7418b734
Kazuya Takei <myself@attakei.net>
2025-05-16 03:29:53 +0900

    fix: Accept filepaths

--- scripts/build-renovate.mjs
+++ scripts/build-renovate.mjs
@@ -6,11 +6,7 @@ import { glob } from "glob";
 import { program } from "commander";
 import yaml from "yaml";
 
-program.argument("<string>");
-
-program.parse();
-for (const src of glob.globSync(`${program.args[0]}/*.yaml`)) {
-  const dest = src.replace(/\.yaml$/, ".json");
+function compile(src, dest) {
   console.debug(`Build from ${src}\n  to ${dest}`);
   const config = yaml.parse(readFileSync(src, { encoding: "utf8" }));
   if (config.customManagers) {
@@ -22,4 +18,18 @@ for (const src of glob.globSync(`${program.args[0]}/*.yaml`)) {
     }
   }
   writeFileSync(dest, JSON.stringify(config, null, 2));
+}
+
+program.argument("[string]").parse();
+
+if (program.args.length > 0) {
+  for (const src of program.args) {
+    const dest = src.replace(/\.yaml$/, ".json");
+    compile(src, dest);
+  }
+} else {
+  for (const src of glob.globSync("renovate/*.yaml")) {
+    const dest = src.replace(/\.yaml$/, ".json");
+    compile(src, dest);
+  }
 }
